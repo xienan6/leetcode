15396aa27b0c153ef5d245f30ae46ad36aa6c883
xienan6 <xienan6@gmail.com>
2018-12-04 21:44:29 +0800

    add twoSum explanation

--- cpp/twoSum/twoSum.cpp
+++ cpp/twoSum/twoSum.cpp
@@ -2,6 +2,13 @@
 // Author : Nn
 // Date   : 2018-12-04
 
+/*
+* The easy solution is use a O(n^2) run-time complexity method. However,
+* we can use a map or a hashmap to store numbers and reduce run-time
+* complexity to O(nlogn) or O(n) because a hashmap or a map can find
+* the number = target - number[0] in O(logn) or O(1).
+*/
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
