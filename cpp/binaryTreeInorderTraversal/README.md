5ab54dfef4a969a3ae94f24531513d10f9381da5
xienan6 <xienan6@gmail.com>
2019-03-22 10:30:09 +0800

    add #94 solutions

---
+++ cpp/binaryTreeInorderTraversal/README.md
@@ -0,0 +1,2 @@
+Use stack in place of recursive solution. Visit p until it has no left child.      
+Time complexity O(n), Space complexity O(n)
---
+++ cpp/binaryTreeInorderTraversal/binaryTreeInorderTraversal.cpp
@@ -0,0 +1,39 @@
+// Source : https://leetcode.com/problems/binary-tree-inorder-traversal/
+// Author : Nan
+// Date   : 2019-03-22
+// Runtime: 8 ms
+
+/*
+* Use stack in place of recursive solution. Visit 
+* p until it has no left child.           
+*
+* Time complexity O(n), Space complexity O(n)
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> s;
+        TreeNode *p = root;
+        while (p || !s.empty()) {
+            while (p) {
+                s.push(p);
+                p = p->left;
+            }
+            p = s.top(); s.pop();
+            res.push_back(p->val);
+            p = p->right;
+        }
+        return res;
+    }
+};
