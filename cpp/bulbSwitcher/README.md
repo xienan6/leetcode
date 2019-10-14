4f9568b700debb5da1525108329a09abe0c7df03
xienan6 <xienan6@gmail.com>
2019-10-14 09:34:57 +0800

    add #319 solution

---
+++ cpp/bulbSwitcher/README.md
@@ -0,0 +1,2 @@
+Find numbers whose factors that are odd, which are complete square number.  
+Time complexity O(1), Space complexity O(1)
---
+++ cpp/bulbSwitcher/bulbSwitcher.cpp
@@ -0,0 +1,17 @@
+// Source : https://leetcode.com/problems/bulb-switcher/
+// Author : Nan
+// Date   : 2019-10-14
+// Runtime: 4 ms
+
+/*
+* Find numbers whose factors that are odd, which are complete square number.
+*
+* Time complexity O(1), Space complexity O(1)
+*/
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return (int)sqrt(n);
+    }
+};
