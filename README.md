1c5b64130314feb60fe91ed7244931575f54bbd5
xienan6 <xienan6@gmail.com>
2019-11-19 12:16:55 +0800

    add #341 solution

--- README.md
+++ README.md
@@ -270,6 +270,7 @@ LeetCode
 |336|[Palindrome Pairs](https://leetcode.com/problems/palindrome-pairs/)| [C++](https://github.com/xienan6/leetcode/tree/master/cpp/palindromePairs/palindromePairs.cpp)|Hard|  
 |337|[House Robber III](https://leetcode.com/problems/house-robber-iii/)| [C++](https://github.com/xienan6/leetcode/tree/master/cpp/houseRobberIII/houseRobberIII.cpp)|Medium|  
 |338|[Counting Bits](https://leetcode.com/problems/counting-bits/)| [C++](https://github.com/xienan6/leetcode/tree/master/cpp/countingBits/countingBits.cpp)|Medium|  
+|341|[Flatten Nested List Iterator](https://leetcode.com/problems/flatten-nested-list-iterator/)| [C++](https://github.com/xienan6/leetcode/tree/master/cpp/flattenNestedListIterator/flattenNestedListIterator.cpp)|Medium|  
 |342|[Power of Four](https://leetcode.com/problems/power-of-four/)| [C++](https://github.com/xienan6/leetcode/tree/master/cpp/powerOfFour/powerOfFour.cpp)|Easy|  
 |343|[Integer Break](https://leetcode.com/problems/integer-break/)| [C++](https://github.com/xienan6/leetcode/tree/master/cpp/integerBreak/integerBreak.cpp)|Medium|  
 |344|[Reverse String](https://leetcode.com/problems/reverse-string/)| [C++](https://github.com/xienan6/leetcode/tree/master/cpp/reverseString/reverseString.cpp)|Easy|  
---
+++ cpp/flattenNestedListIterator/flattenNestedListIterator.cpp
@@ -0,0 +1,58 @@
+// Source : https://leetcode.com/problems/flatten-nested-list-iterator/
+// Author : Nan
+// Date   : 2019-11-19
+// Runtime: 20 ms
+
+/*
+* DFS.
+*
+* Time complexity O(n), Space complexity O(n)
+*/
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class NestedIterator {
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        dfs(nestedList);
+    }
+
+    int next() {
+        return v[j++];
+    }
+
+    bool hasNext() {
+        return j < v.size();
+    }
+    
+    void dfs(vector<NestedInteger> &nestedList) {
+        for (int i = 0; i < nestedList.size(); ++i) {
+            if (nestedList[i].isInteger()) v.push_back(nestedList[i].getInteger());
+            else dfs(nestedList[i].getList());
+        }
+    }
+private:
+    vector<int> v;
+    int j = 0;
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
